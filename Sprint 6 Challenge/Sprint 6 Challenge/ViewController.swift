1644428549d5963ef583817fac975c76a09910c4
Alex <alexcc17@gmail.com>
2019-05-17 11:40:18 -0400

    Add storyboard

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var fullView: UIView!
+    @IBOutlet var resetBtn: UIBarButtonItem!
+    @IBOutlet var lockImgView: UIImageView!
+    @IBOutlet var sliderView: UIView!
+    @IBOutlet var sliderViewContainer: UIView!
+    
+    @IBAction func resetBtnPressed(_ sender: UIBarButtonItem) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
