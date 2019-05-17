ec8c6c2753b68eada77cfa5d218450831fa300da
Alex <alexcc17@gmail.com>
2019-05-17 11:50:38 -0400

    Add CustomControl class

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
@@ -0,0 +1,24 @@
+//
+//  CustomControl.swift
+//  Sprint 6 Challenge
+//
+//  Created by Alex on 5/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        <#code#>
+    }
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        <#code#>
+    }
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        <#code#>
+    }
+    override func cancelTracking(with event: UIEvent?) {
+        <#code#>
+    }
+}
